26439eafa6c51d3bd1e810fcfe14abd84cd4e702
Ahmed3116 <ahmedmo7amed1616@gmail.com>
2026-07-29 13:04:47 +0300

    Add files via upload

---
+++ simple_calculator.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1,num2;
+    char operation;
+    cout << "ente the first number:";
+    cin >> num1;
+    cout << "ente the second number:";
+    cin >> num2;
+    cout << "ente the operation:";
+    cin >> operation;
+    if (operation == '+')
+        cout << num1<< "+"<< num2<<"=" << num1 + num2;
+    else if (operation == '-')
+        cout << num1 << "-" << num2 << "=" << num1 - num2;
+    else if (operation == '/')
+        cout << num1 << "/" << num2 << "=" << num1 / num2;
+    else if (operation == '*')
+        cout << num1 << "*" << num2 << "=" << num1 * num2;
+
+
+}
